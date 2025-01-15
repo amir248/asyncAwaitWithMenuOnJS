5351abb017fab9ab82f8cd1e394a791a6ed88ef9
chikchicly <chikchicly@gmail.com>
2025-01-16 06:39:00 +0700

    Amir Dzhan

--- site/js/menu.js
+++ site/js/menu.js
@@ -1,11 +1,11 @@
-const textMenu="<h3><a href='#one'>one list</a></h3><h3><a href='#two'>two list</a></h3><h3><a href='#three'>three list</a></h3>";
+const textMenu="<h3><a href='#one'>one list</a></h3><h3><a href='#two'>two list</a></h3><h3><a href='#three'>three list</a></h3><h3><a href='https://wa.me/79528885656'>Amir Dzhan</a></h3>";
 document.querySelector('#menu').addEventListener('click',()=>{
     const openMenu=document.createElement('div');
     openMenu.classList.add('openMenu');
     openMenu.innerHTML=`${textMenu}`;
     document.querySelector('body').prepend(openMenu);
     setTimeout(()=>{
-        document.querySelector('.openMenu').style.cssText=`width:100%;font-size:33px;`
+        document.querySelector('.openMenu').style.cssText=`width:100%;font-size:33px;opacity:1;`;
     },300);
     closeMenu();
 });
